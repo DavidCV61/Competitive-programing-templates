70c4d5f5c5d8677c833b8042584c896b0e1e3beb
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2022-09-11 19:52:34 -0500

    Create String.cpp

---
+++ Arrray/String.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+
+
+using namespace std; 
+
+int main(){
+
+string s="Santiago";
+int tam=s.size();
+cout<<tam<<endl;
+char c=s[0];
+cout<<c<<endl;
+s=s+" Ormeño";
+cout<<s<<endl;
+
+s.push_back('z');
+cout<<s<<endl;
+s.pop_back();
+cout<<s<<endl;
+string r=s.substr(6,8);
+cout<<r<<endl;
+//cin<<s;
+//getline(cin,s);
+//while(cin<<s;)
+//while(getline(cin,s))
+
+
+
+
+
+return 0;
+}
+
