2a352d10b99825da3384fe4236e681125efcbcf6
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2022-09-11 19:22:43 -0500

    Create Stack.cpp

---
+++ STL/Stack.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+stack<int> st;
+st.push(5);
+st.push(3);
+st.push(-1);
+st.push(7);
+while(!st.empty()){
+cout<<st.top()<<endl;
+st.pop();
+
+
+}
+
+
+
+
+    return 0;
+}
