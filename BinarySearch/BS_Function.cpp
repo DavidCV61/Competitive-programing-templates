1ed220782f61e113a0a150a15e0455e0d5a5c2c2
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2022-09-11 20:13:13 -0500

    Create BS_Function.cpp

---
+++ BinarySearch/BS_Function.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+ 
+    
+vector <int> v={1,3,4,5,7,15};
+
+int x=2;
+ 
+        if(binary_search(v.begin(),v.end(),x)){
+ 
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+ 
+ 
+        }
+
+
+ x=3;
+         if(binary_search(v.begin(),v.end(),x)){
+ 
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+ 
+ 
+        }
+    
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    return 0;
+ 
+}
